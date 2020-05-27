d98771d83c318f653151c4b906f42df28659bbd2
Rakiden <38255909+Rakiden@users.noreply.github.com>
2020-05-27 14:04:07 +0200

    Delete config.php

--- server_api/config/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('DB_NAME','sql7343494');
-define('DB_USER','sql7343494');
-define('DB_PASSWORD','2v8U3xX3t4');
-define('DB_HOST','sql7.freemysqlhosting.net');
-
-$mysqli = new mysqli(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
-?>
